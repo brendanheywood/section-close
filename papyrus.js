c54657a354d0460b1c8f3c7d6632673bfe986c43
Brendan Heywood <brendan.heywood@gmail.com>
2012-11-08 01:21:26 +1000

    started conversion to plugin

---
+++ papyrus.js
@@ -0,0 +1,59 @@
+;(function ( $, window, undefined ) {
+
+
+var heading = 'h1';
+var body = '.body';
+var delay = 500;
+var message = 'Close this section';
+var openClass = 'open';
+var alwaysScroll = true;
+
+
+	//open from top
+	$('section').find(heading).click(function topToggle(){
+		var sec = $(this).closest('section');
+		if (sec.hasClass(openClass)){
+			sec.removeClass(openClass);
+			sec.find(body).slideUp(delay);
+		} else {
+			sec.addClass(openClass);
+			sec.find(body).slideDown(delay);
+		}
+	});
+
+
+	var $close = $('<div class="close">' + message + '</div>').click(function bottomClose(){
+
+		var sec = $(this).closest('section');
+		sec.removeClass(openClass);
+
+		var scrollTop = $('body').scrollTop();
+		var sectionTop    = sec.position().top;
+
+		sec.find(body).slideUp(delay);
+
+		// If the heading is visible then just let is slide up
+		if (!alwaysScroll && sectionTop > scrollTop){ return; }
+  
+		// If the heading is not visible then slide the top down instead of the bottom up
+		var sectionHeight = sec.find(body).outerHeight();
+
+		// animate a mirror opening div
+		var $cheat = $('<div> </div>')
+			.prependTo('body')
+			.css('height', 0)
+			.animate({
+				'height': sectionHeight
+			}, delay, function(){
+				// remove dummy
+				$cheat.remove();
+				window.scrollTo(0, scrollTop - sectionHeight); // TODO what about x dimension?
+			});
+	});
+
+
+	$('section').find(body)
+		.append( $close )
+		.hide();
+
+}(jQuery, window));
